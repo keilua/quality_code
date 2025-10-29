9c8b1660ec38608e0201ca2d3a331c16893fa7b2
tr00d <guillaume.faas@live.fr>
2025-10-29 06:59:17 +0100

    feat: add tests for phone number verification

--- CodeQuality.Samples/Quality/PhoneNumberVerification.cs
+++ CodeQuality.Samples/Quality/PhoneNumberVerification.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+
 namespace CodeQuality.Samples.Quality;
 
 public class PhoneNumberVerification
@@ -16,4 +18,36 @@ public class PhoneNumberVerification
 
         return true;
     }
+}
+
+public class PhoneNumberVerificationTest
+{
+    [Fact]
+    public void ShouldReturnFalse_GivenValueIsNull() => PhoneNumberVerification.v(null).Should().BeFalse();
+    
+    [Fact]
+    public void ShouldReturnFalse_GivenValueIsEmpty() => PhoneNumberVerification.v(string.Empty).Should().BeFalse();
+    
+    [Theory]
+    [InlineData("1")]
+    [InlineData("12")]
+    [InlineData("123")]
+    [InlineData("1234")]
+    [InlineData("12345")]
+    [InlineData("123456")]
+    [InlineData("1234567")]
+    public void ShouldReturnFalse_GivenLengthIsLowerThan8(string input) => PhoneNumberVerification.v(input).Should().BeFalse();
+    
+    [Theory]
+    [InlineData("1234567890123456")]
+    public void ShouldReturnFalse_GivenLengthIsHigherThan15(string input) => PhoneNumberVerification.v(input).Should().BeFalse();
+    
+    [Fact]
+    public void ShouldReturnFalse_GivenDoesNotStartWithPlusSign() => PhoneNumberVerification.v("123456789").Should().BeFalse();
+    
+    [Fact]
+    public void ShouldReturnFalse_GivenNotAllCharactersAreNumerics() => PhoneNumberVerification.v("+1a34!6789").Should().BeFalse();
+    
+    [Fact]
+    public void ShouldReturnTrue_GivenValueIsValidE164() => PhoneNumberVerification.v("+123456789").Should().BeTrue();
 }
